88b4beda8b2a1016be4ce11574c00190ef1eca8f
IlyassGueddi <gueddiilyass99@gmail.com>
2025-09-19 09:45:51 +0100

    ilyass

---
+++ nano-banana.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-576c967acb1d4e2d93d6fc324cedee9b", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
