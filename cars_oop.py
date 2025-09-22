0ede2ae9ba5ead27f032d36410c9d6c39621aea4
IlyassGueddi <gueddiilyass99@gmail.com>
2025-09-22 10:31:42 +0100

    cars project using OOP

---
+++ cars_oop.py
@@ -0,0 +1,39 @@
+class car:
+    def __init__(self, brand, color, speed, doors):
+        self.brand = brand
+        self.color = color
+        self._speed = speed
+        self.doors = doors
+    def drive(self):
+        print(f"the {self.color} {self.brand} car going with a speed of {self._speed}.")
+    def stop(self):
+        print(f"the {self.color} {self.brand} car stops.")
+    def accelerate(self, amount):
+        self._speed += amount
+        
+class electricar(car):
+    def drive(self):
+        print(f"the {self.color} {self.brand} car going with a speed of {self._speed}.")
+    def stop(self):
+        print(f"the {self.color} {self.brand} car stops.")
+    def accelerate(self, amount):
+        self._speed += amount
+
+class sportcar(car):
+    def drive(self):
+        print(f"the {self.color} {self.brand} car going with a speed of {self._speed}.")
+    def stop(self):
+        print(f"the {self.color} {self.brand} car stops.")
+    def accelerate(self, amount):
+        self._speed += amount
+
+my_car = car("dacia", "blue", 80, 4)
+panamera = sportcar("porche", "mateblack", 260, 2)
+cybertruck = electricar("tesla", "gray", 150, 4)
+
+cars = [my_car, panamera, cybertruck]
+for car in cars:
+    car.drive()
+    car.accelerate(10)
+    car.drive()
+    car.stop()
--- qr-code.py
+++ qr-code.py
@@ -2,15 +2,15 @@ import qrcode
 from qrcode.constants import ERROR_CORRECT_H
 
 qr = qrcode.QRCode(
-    version=1,  # controls size (1 is smallest, 40 is largest)
-    error_correction=ERROR_CORRECT_H,  # how much damage can be tolerated
-    box_size=10,  # size of each “box” in pixels
-    border=4,     # thickness of border (in boxes)
+    version=1,
+    error_correction=ERROR_CORRECT_H,
+    box_size=10,
+    border=4,     
 )
 
 source = input("what you want to encode: ")
 
-qr.add_data(source)  # what to encode
+qr.add_data(source)
 qr.make(fit=True)
  
 img = qr.make_image(fill_color="black", back_color="white")
