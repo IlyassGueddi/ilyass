984f06e0564908e18971ba05431a3132cea3462c
IlyassGueddi <gueddiilyass99@gmail.com>
2025-09-22 08:17:56 +0100

    ilyass

--- number_guessing.py
+++ number_guessing.py
@@ -4,18 +4,20 @@ import random
 
 while True :
     pc = random.randint(1, 100)
-    user = int(input("what you think the number is : "))
-    if user > pc :
-        print("too hight!")
-    elif user < pc :
-        print("too low!")
-    elif user == pc :
-        print(f"you win the number is {pc}.")
-        answer = input("want you to play again? (y/n): ")
-        if answer == "y" or answer == "Y":
-            continue
-        elif answer == "n" or answer == "N":
-            print("GoodBye")
+    while pc:
+        user = int(input("what you think the number is : "))
+        if user > pc :
+            print("too hight!")
+        elif user < pc :
+            print("too low!")
+        elif user == pc :
+            print(f"you win the number is {pc}.")
             break
-    else:
-        print("invalide value")
+        else:
+            print("invalide value")
+    answer = input("want you to play again? (y/n): ")
+    if answer == "y" or answer == "Y":
+        continue
+    elif answer == "n" or answer == "N":
+        print("GoodBye")
+        break
