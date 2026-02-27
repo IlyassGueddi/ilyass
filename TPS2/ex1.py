59146f156208749a6317caec4526aead14a61e95
Mial <gueddiilyass99@gmail.com>
2026-02-27 09:35:31 +0000

    ex1

--- TPS2/ex1.py
+++ TPS2/ex1.py
@@ -3,3 +3,17 @@ mon_tuple = (10, 20, ma_liste)
 
 mon_tuple[2].append(4)
 print(mon_tuple)
+
+#ma_liste is just a pointer so it's not considered as a value of a tuple so we can change it ma_liste
+
+
+
+etudiant_A = {"nom": "Ali", "notes": [12, 14]}
+etudiant_B = etudiant_A
+etudiant_B["nom"] = "Sara"
+etudiant_B["notes"].append(16)
+
+print(etudiant_A["nom"], etudiant_A["notes"])
+
+#in python etudiant_B = etudiant_A did not copy etudiant_A to etudiant_B 
+#unlike other languages like C
